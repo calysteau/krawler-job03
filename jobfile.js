e106be4aa60b820bd4b33b446cfa7665a25a1017
Calysteau <91131962+calysteau@users.noreply.github.com>
2022-06-29 13:29:20 +0200

    Add files via upload

---
+++ jobfile.js
@@ -0,0 +1,85 @@
+import moment from 'moment'
+import path from 'path'
+import { hooks } from '@kalisio/krawler'
+
+const imagePattern = 'YYYYMMDD_HHmm_[Radar].png'
+const storePattern = 'YYYY/MM/DD/HHmm.tif'
+const storePath = process.env.STORE_PATH
+const frequency = 900000    // every 15 minutes
+const history = 12
+
+const outputDir = './output'
+
+// Create a custom hook to generate tasks
+let generateTasks = (options) => {
+    return function (hook) {
+        // Compute the nearest round time fron 'now'
+        let nearestTime = Math.floor(moment().utc() / frequency) * frequency
+        // Compute the tasks to be performed according the history
+        let tasks = []
+        for (let i = 4; i < history + 4; i++) {
+            const time = moment(nearestTime - i * frequency).utc()
+            const imageName = time.format(imagePattern)
+            console.log('processing ', imageName)
+            tasks.push({
+                id: imageName,
+                options: {
+                    url: 'https://donneespubliques.meteofrance.fr/donnees_libres/Carto/' + imageName,
+                    inputFile: path.join(outputDir, imageName),
+                    outputFile: path.join(outputDir, imageName.replace('png', 'tif')),
+                    fsKey: imageName.replace('png', 'tif'),
+                    storeKey: time.format(storePattern)
+                }
+            })
+        }
+        hook.data.tasks = tasks
+        return hook
+    }
+}
+hooks.registerHook('generateTasks', generateTasks)
+
+export default {
+    id: 'meteoradar',
+    store: 'fs',
+    options: {
+        workersLimit: 1,
+        faultTolerant: true
+    },
+    taskTemplate: {
+        type: 'http',
+        faultTolerant: true
+    },
+    hooks: {
+        tasks: {
+            after: {
+                gdalTransform: {
+                    hook: 'runCommand',
+                    command: '"./transform.sh" "<%= options.inputFile %>" "<%= options.outputFile %>"'
+                },
+                rcloneCopy: {
+                    hook: 'runCommand',
+                    match: { predicate: () => storePath },
+                    command: `rclone copy ${outputDir}/<%= options.fsKey %> store:${storePath}/<%= options.storeKey %>`
+                },
+            },
+        },
+        jobs: {
+            before: {
+                createStore: {
+                    id: 'fs',
+                    options: {
+                        path: outputDir
+                    },
+                    storePath: 'taskTemplate.store'
+                },
+                generateTasks: {}
+            },
+            after: {
+                removeStore: ['fs']
+            },
+            error: {
+                removeStore: ['fs']
+            }
+        }
+    }
+}
---
+++ transform.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# https://gis.stackexchange.com/questions/387111/is-it-possible-to-figure-out-the-projection-used-in-a-map
+
+PNG_FILE=$1
+TIF_FILE=$2
+
+# -multi -wo NUM_THREADS=ALL_CPUS -> multithread reproject
+# -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS -> deflate compression + multithread compression
+# -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co COPY_SRC_OVERVIEWS=YES -> cloud optimized generation options
+
+# https://gdal.org/drivers/raster/gtiff.html
+# https://gdal.org/drivers/raster/cog.html
+# https://gdal.org/programs/gdalwarp.html
+# https://gdal.org/programs/gdal_translate.html
+# https://gdal.org/programs/gdaladdo.html
+# https://github.com/cogeotiff/cog-spec/blob/master/spec.md
+
+# png to geotiff with proper georeferencing
+gdal_translate \
+  -a_srs "+title=thomas +proj=stere +lat_0=90 +lat_ts=45 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs" \
+  -a_ullr -619652.074 -3526818.338 916347.926 -5062818.338 \
+  -a_nodata 0 \
+  -of GTiff -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co COPY_SRC_OVERVIEWS=YES \
+  ${PNG_FILE} ${TIF_FILE}.tmp
+
+if [ $? -ne 0 ]; then
+   echo "Error while applying gdal_translate"
+   rm ${PNG_FILE}
+   exit 1
+fi
+
+# project to 4326
+gdalwarp \
+  -overwrite \
+  -t_srs EPSG:4326 \
+  -of GTiff -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co COPY_SRC_OVERVIEWS=YES \
+  -co COMPRESS=DEFLATE \
+  ${TIF_FILE}.tmp ${TIF_FILE}
+
+if [ $? -ne 0 ]; then
+   echo "Error while applying gdalwarp"
+   rm ${PNG_FILE}
+   rm ${TIF_FILE}.tmp
+   exit 1
+fi
+
+rm ${TIF_FILE}.tmp
